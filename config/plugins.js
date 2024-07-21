6f0eb37900c651b0364072e59857e25fc5e04a78
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-21 05:19:32 -0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,57 @@
-module.exports = () => ({});
+module.exports = {
+  "strapi-google-auth": {
+    enabled: true,
+  },
+ {
+  "strapi-html-editor": {
+    enabled: true,
+  }, 
+  {
+  "netlify-deployments": {
+    enabled: true,
+  },
+  {
+  "strapi-stripe": {
+    enabled: true,
+  },
+  
+ {
+  "strapi-plugin-website-builder": {
+    enabled: true,
+  },
+
+  {
+  "vercel-deploy": {
+    enabled: true,
+  },
+
+  {
+  // ...
+  'sitemap': {
+    enabled: true,
+    config: {
+      cron: '0 0 0 * * *',
+      limit: 45000,
+      xsl: true,
+      autoGenerate: false,
+      caching: true,
+      allowedFields: ['id', 'uid'],
+      excludedTypes: [],
+    },
+  },
+  {
+  "strapi-plugin-rest-cache": {
+    enabled: true,
+  },
+
+  {
+  // ...
+  'open-ai': {
+    enabled: true,
+    config: {
+      API_TOKEN: '<your-open-ai-token>',
+    },
+  },
+  
+};
+
