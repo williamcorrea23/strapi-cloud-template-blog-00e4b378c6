52fa32365e5ce657ea90bfe697f7e444d0691264
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-21 05:44:50 -0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,32 +1,29 @@
-module.exports = {
+module.exports = ({ env }) => ({
   "strapi-google-auth": {
     enabled: true,
   },
- {
   "strapi-html-editor": {
     enabled: true,
-  }, 
-  {
+  },
   "netlify-deployments": {
     enabled: true,
   },
-  {
   "strapi-stripe": {
     enabled: true,
   },
-  
- {
   "strapi-plugin-website-builder": {
     enabled: true,
   },
-
-  {
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook: env('VERCEL_DEPLOY_PLUGIN_HOOK'),
+      apiToken: env('VERCEL_DEPLOY_PLUGIN_API_TOKEN'),
+      appFilter: env('VERCEL_DEPLOY_PLUGIN_APP_FILTER'),
+      teamFilter: env('VERCEL_DEPLOY_PLUGIN_TEAM_FILTER'),
+      roles: ["strapi-super-admin"],
+    },
   },
-
-  {
-  // ...
   'sitemap': {
     enabled: true,
     config: {
@@ -39,36 +36,38 @@ module.exports = {
       excludedTypes: [],
     },
   },
-  {
   "strapi-plugin-rest-cache": {
     enabled: true,
   },
-
-  {
-  // ...
   'open-ai': {
     enabled: true,
     config: {
-      API_TOKEN: '<your-open-ai-token>',
+      API_TOKEN: env('OPEN_AI_TOKEN'),
     },
   },
-  {
-  // ...
   "strapi-chatgpt": {
     enabled: true,
   },
-
-  {
   ckeditor5: {
     enabled: true,
     resolve: "./src/plugins/strapi-plugin-ckeditor"
   },
-
-  {
-  // ...
   'drag-drop-content-types': {
-    enabled: true
-  }
-},
-};
-
+    enabled: true,
+  },
+  "firebase-auth": {
+    enabled: true,
+    config: {
+      FIREBASE_JSON_ENCRYPTION_KEY: env('FIREBASE_JSON_ENCRYPTION_KEY'),
+    },
+  },
+  "strapi-plugin-graphs-builder": {
+    enabled: true,
+  },
+  'import-export-entries': {
+    enabled: true,
+    config: {
+      // Configurações adicionais
+    },
+  },
+});
