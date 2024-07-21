139ba8a7501dca1cfba990ce72019a958acaeb8a
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-21 06:03:24 -0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -48,9 +48,9 @@ module.exports = ({ env }) => ({
   "strapi-chatgpt": {
     enabled: true,
   },
-  ckeditor5: {
+  "ckeditor5": {
     enabled: true,
-    resolve: "./src/plugins/strapi-plugin-ckeditor"
+    resolve: "@strapi/plugin-ckeditor5",
   },
   'drag-drop-content-types': {
     enabled: true,
